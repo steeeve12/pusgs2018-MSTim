c55787534ad742bf5292f40e9de3c75499321e92
Mitar95 <blackshnit@gmail.com>
2018-06-05 09:30:57 +0200

    dodati kontroleri

---
+++ initialApp-master/RentApp/Controllers/BranchesController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using RentApp.Models.Entities;
+using RentApp.Persistance;
+
+namespace RentApp.Controllers
+{
+    public class BranchesController : ApiController
+    {
+        private RADBContext db = new RADBContext();
+
+        // GET: api/Branches
+        public IQueryable<Branch> GetBranches()
+        {
+            return db.Branches;
+        }
+
+        // GET: api/Branches/5
+        [ResponseType(typeof(Branch))]
+        public IHttpActionResult GetBranch(int id)
+        {
+            Branch branch = db.Branches.Find(id);
+            if (branch == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(branch);
+        }
+
+        // PUT: api/Branches/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutBranch(int id, Branch branch)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != branch.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(branch).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BranchExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Branches
+        [ResponseType(typeof(Branch))]
+        public IHttpActionResult PostBranch(Branch branch)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Branches.Add(branch);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = branch.Id }, branch);
+        }
+
+        // DELETE: api/Branches/5
+        [ResponseType(typeof(Branch))]
+        public IHttpActionResult DeleteBranch(int id)
+        {
+            Branch branch = db.Branches.Find(id);
+            if (branch == null)
+            {
+                return NotFound();
+            }
+
+            db.Branches.Remove(branch);
+            db.SaveChanges();
+
+            return Ok(branch);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool BranchExists(int id)
+        {
+            return db.Branches.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ initialApp-master/RentApp/Controllers/VehicleTypesController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using RentApp.Models.Entities;
+using RentApp.Persistance;
+
+namespace RentApp.Controllers
+{
+    public class VehicleTypesController : ApiController
+    {
+        private RADBContext db = new RADBContext();
+
+        // GET: api/VehicleTypes
+        public IQueryable<VehicleType> GetVehicleTypes()
+        {
+            return db.VehicleTypes;
+        }
+
+        // GET: api/VehicleTypes/5
+        [ResponseType(typeof(VehicleType))]
+        public IHttpActionResult GetVehicleType(int id)
+        {
+            VehicleType vehicleType = db.VehicleTypes.Find(id);
+            if (vehicleType == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(vehicleType);
+        }
+
+        // PUT: api/VehicleTypes/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutVehicleType(int id, VehicleType vehicleType)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != vehicleType.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(vehicleType).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VehicleTypeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/VehicleTypes
+        [ResponseType(typeof(VehicleType))]
+        public IHttpActionResult PostVehicleType(VehicleType vehicleType)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.VehicleTypes.Add(vehicleType);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = vehicleType.Id }, vehicleType);
+        }
+
+        // DELETE: api/VehicleTypes/5
+        [ResponseType(typeof(VehicleType))]
+        public IHttpActionResult DeleteVehicleType(int id)
+        {
+            VehicleType vehicleType = db.VehicleTypes.Find(id);
+            if (vehicleType == null)
+            {
+                return NotFound();
+            }
+
+            db.VehicleTypes.Remove(vehicleType);
+            db.SaveChanges();
+
+            return Ok(vehicleType);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool VehicleTypeExists(int id)
+        {
+            return db.VehicleTypes.Count(e => e.Id == id) > 0;
+        }
+    }
+}
