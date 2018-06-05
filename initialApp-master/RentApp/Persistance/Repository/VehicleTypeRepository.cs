e664b2d97c0dc4678a8516f4008c67d446677635
Mitar95 <blackshnit@gmail.com>
2018-06-05 10:41:55 +0200

    dodate Repository clase i interfejsi

---
+++ initialApp-master/RentApp/Persistance/Repository/VehicleTypeRepository.cs
@@ -0,0 +1,23 @@
+﻿using RentApp.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RentApp.Persistance.Repository
+{
+    public class VehicleTypeRepository : Repository<VehicleType, int>, IVehicleTypeRepository
+    {
+        public VehicleTypeRepository(DbContext context) : base(context)
+        {
+        }
+
+        public IEnumerable<VehicleType> GetAll(int pageIndex, int pageSize)
+        {
+            return RADBContext.VehicleTypes.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+        }
+
+        protected RADBContext RADBContext { get { return context as RADBContext; } }
+    }
+}
