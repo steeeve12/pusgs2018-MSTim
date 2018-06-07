22cee9e821528a41e6ccd694f15ae8a9d2f18496
steeeve12 <e33-2014@P05-21>
2018-06-07 15:18:16 +0200

    namesteno rutiranje za home, login i register

--- initialApp-master/Angular/Client-app/src/app/app.module.ts
+++ initialApp-master/Angular/Client-app/src/app/app.module.ts
@@ -1,20 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientXsrfModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { MainComponent } from './main/main.component';
+
+const Routes = [
+{
+  path: '',
+  redirectTo: "/home",
+  pathMatch: 'full'
+},  
+{
+  path: "home",
+  component: HomeComponent,
+},
+{
+  path: "login",
+  component: LoginComponent
+},
+{
+  path: "register",
+  component: RegisterComponent
+}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(Routes),
+    HttpModule,
+    HttpClientModule,
+    HttpClientXsrfModule
   ],
   providers: [],
   bootstrap: [AppComponent]
