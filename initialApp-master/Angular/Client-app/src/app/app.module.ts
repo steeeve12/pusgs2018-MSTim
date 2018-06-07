43a2408fd156406b794110c0abf650109130e6e8
steeeve12 <e33-2014@P05-21>
2018-06-07 14:04:27 +0200

    home i register kontroleri

--- initialApp-master/Angular/Client-app/src/app/app.module.ts
+++ initialApp-master/Angular/Client-app/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
